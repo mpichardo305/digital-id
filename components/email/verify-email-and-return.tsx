3b75fe70578198f32f9011fad68b5a6f56910734
Michael Pichardo <michael.pichardo@reeftechnology.com>
2025-09-04 18:11:23 -0400

    replaced import statement to import react-email/components

--- components/email/verify-email-and-return.tsx
+++ components/email/verify-email-and-return.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // @ts-ignore
-import { Html, Head, Body, Container, Section, Img, Text, Button, Hr, Link } from 'https://esm.sh/@react-email/components@0.0.12';
+import { Html, Head, Body, Container, Section, Img, Text, Button, Hr, Link } from "@react-email/components";
 import { emailStyles as styles } from "./emailStyles.ts";
 
 export default function VerifyEmailAndReturn({
